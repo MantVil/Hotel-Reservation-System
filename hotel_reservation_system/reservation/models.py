a933d7581900a9991204dd28f2f9b2025c9cca02
Mantiena <mantasdagg@gmail.com>
2022-12-08 14:42:01 +0200

    changes in models.py

--- hotel_reservation_system/reservation/models.py
+++ hotel_reservation_system/reservation/models.py
@@ -6,8 +6,15 @@ class Hotel(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+
 class Reservation(models.Model):
     hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
     check_in_date = models.DateField()
     check_out_date = models.DateField()
     num_guests = models.IntegerField()
+
+    def __str__(self):
+        return f'Reservation at {self.hotel.name} from {self.check_in_date} to {self.check_out_date}'
