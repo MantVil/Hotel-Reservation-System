2450382342444f461c507d955e90feaf56277d01
Mantiena <mantasdagg@gmail.com>
2022-12-08 12:07:22 +0200

    updated admin.py panel with classes

--- hotel_reservation_system/reservation/admin.py
+++ hotel_reservation_system/reservation/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from reservation.models import Hotel, Reservation
 
+class HotelAdmin(admin.ModelAdmin):
+    fields = ['name', 'address']
 
-admin.site.register(Hotel)
-admin.site.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    fields = ['hotel', 'check_in_date', 'check_out_date', 'num_guests']
+
+admin.site.register(Hotel, HotelAdmin)
+admin.site.register(Reservation, ReservationAdmin)
