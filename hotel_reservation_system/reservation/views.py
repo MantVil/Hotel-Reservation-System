785f50c72a4361f0127a7291d797721464f53f82
Mantiena <mantasdagg@gmail.com>
2022-12-08 10:47:39 +0200

    views.py first update done.

--- hotel_reservation_system/reservation/views.py
+++ hotel_reservation_system/reservation/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from reservation.models import Hotel, Reservation
 from rest_framework import generics
+from serializers import HotelSerializer, ReservationSerializer
 
 class HotelListView(generics.ListAPIView):
     queryset = Hotel.objects.all()
     serializer_class = HotelSerializer
 
+class HotelDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = HotelSerializer
+
+
+class ReservationListView(generics.ListCreateAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+class ReservationDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset =  Reservation.objects.all()
+    serializer_class = ReservationSerializer
