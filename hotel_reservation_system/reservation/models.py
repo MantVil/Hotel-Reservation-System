1590819a34ff58212c2aeeadc96f75ca37c30cae
Mantiena <mantasdagg@gmail.com>
2022-12-08 10:26:18 +0200

    Serializers.py and models.py update

--- hotel_reservation_system/reservation/models.py
+++ hotel_reservation_system/reservation/models.py
@@ -5,4 +5,9 @@ from django.db import models
 class Hotel(models.Model):
     name = models.CharField(max_lenght=100)
     address = models.CharField(max_lenght=100)
-    
+
+class Reservation(models.Model):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    check_in_date = models.DateField()
+    check_out_date = models.DateField()
+    num_guests = models.IntegerField()
---
+++ hotel_reservation_system/reservation/serializers.py
@@ -0,0 +1,12 @@
+from reservation.models import Hotel, Reservation
+from rest_framework import serializers
+
+class HotelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hotel
+        field = ('id', 'name', 'address')
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = ('id', 'hotel', 'check_in_date', 'check_out_date', 'guest_number')
