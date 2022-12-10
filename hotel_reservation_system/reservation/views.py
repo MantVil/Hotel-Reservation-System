d431b19e980f2cc00fe049b4a7f0a67deb3e2e01
Mantiena <mantasdagg@gmail.com>
2022-12-10 21:35:26 +0200

    update user/ and detailview user completed

--- hotel_reservation_system/reservation/views.py
+++ hotel_reservation_system/reservation/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from reservation.models import Hotel, Reservation
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Hotel, RoomCategory, Reservation
-from .serializers import HotelSerializer, RoomCategorySerializer, ReservationSerializer, UserRegistrationSerializer
+from .serializers import HotelSerializer, RoomCategorySerializer, ReservationSerializer, UserRegistrationSerializer, UserSerializer
 
 class HotelListView(generics.ListAPIView):
     queryset = Hotel.objects.all()
@@ -26,7 +27,6 @@ class ReservationListView(generics.ListAPIView):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
 
-
 class ReservationCreateView(generics.CreateAPIView):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
@@ -34,3 +34,25 @@ class ReservationCreateView(generics.CreateAPIView):
 
 class UserRegistrationView(generics.CreateAPIView):
     erializer_class = UserRegistrationSerializer
+
+class UserDetailView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+class UserUpdateView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_update(self, serializer):
+        user = self.request.user
+        password = self.request.data.get('password')
+
+        if password and user.id == self.kwargs['pk']:
+            if not user.check_password(password):
+                raise ValidationError({'password': 'Incorrect password'})
+
+        serializer.save()
+
+class UserDestroyView(generics.DestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
