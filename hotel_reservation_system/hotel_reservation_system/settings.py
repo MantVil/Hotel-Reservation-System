65c8a420bea8b0e4d5021355ac598a915d6a3a93
Mantiena <mantasdagg@gmail.com>
2022-12-11 15:01:38 +0200

    settings.py correction

--- hotel_reservation_system/hotel_reservation_system/settings.py
+++ hotel_reservation_system/hotel_reservation_system/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'reservation',
     'rest_framework'
+    'rest_framewrok.authtoken',
     
 
  
@@ -128,3 +129,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
--- hotel_reservation_system/reservation/urls.py
+++ hotel_reservation_system/reservation/urls.py
@@ -6,9 +6,10 @@ urlpatterns = [
     path('hotel/<int:pk>', views.HotelDetailView.as_view()),
     path('hotel/room-categories/', views.RoomCategoryListView.as_view()),
     path('hotel/room-categories/<int:pk>', views.RoomCategoryDetailView.as_view()),
-    path('hotel/reservations/', views.ReservationListView.as_view()),
-    path('hotel/reservations/create/', views.ReservationCreateView.as_view()),
-    path('user/register/', views.UserRegistrationView.as_view(), name='user registration'),
+    path('hotel/reservations/', views.ReservationListView.as_view(), name='reservation-list'),
+    path('hotel/reservations/<int:pk>/', views.ReservationDetailView.as_view()),
+    path('user/register/', views.UserRegistrationView.as_view(), name='user-registration'),
     path('users/<int:pk>/', views.UserDetailView.as_view(), name = 'user-detail'),
-    path('users/<int:pk>/update', views.UserUpdateView.as_view(), name = 'user-update')
+    path('users/<int:pk>/update', views.UserUpdateView.as_view(), name = 'user-update'),
+    path('users/', views.UserDestroyView.as_view(), name = 'user-destroy')
 ]
