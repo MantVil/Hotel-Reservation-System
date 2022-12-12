0069a107eed6ab74f26743328f0a888f037dbf4e
Mantiena <mantasdagg@gmail.com>
2022-12-12 23:31:48 +0200

    re-built serializers.py and views.py

--- hotel_reservation_system/reservation/serializers.py
+++ hotel_reservation_system/reservation/serializers.py
@@ -1,86 +1,17 @@
-# from rest_framework import serializers
-# from django.contrib.auth.models import User
-# from .models import Hotel, RoomCategory, Reservation
-
-
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ('username', 'password', 'email')
-#         extra_kwargs = {'password': {'write_only': True}}
-
-# class HotelSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Hotel
-#         fields = ['id', 'name', 'address']
-
-# class RoomSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = RoomCategory
-#         fields = ['id', 'name', 'hotel', 'max_occupacy']
-
-# class ReservationSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Reservation
-#         fields = ['id', 'hotel', 'user', 'room_category', 'check_in_date', 'check_out_date', 'num_guests']
-
-
-
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from django.core.exceptions import ValidationError
 from .models import Hotel, RoomCategory, Reservation
 
 class HotelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hotel
-        fields = ['id', 'name', 'address']
+        fields = ('name', 'address')
 
 class RoomCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = RoomCategory
-        fields = ['id', 'name', 'hotel', 'max_occupacy']
-
-class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    email = serializers.CharField(required=True)
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'password', 'email']
-
-    def create(self, validated_data):
-        user = User.objects.create(
-            username = validated_data['username'],
-            email = validated_data['email']
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
-
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only = True)
-    token = serializers.CharField(read_only=True)
-    user_id = serializers.IntegerField(read_only=True)
-    
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'password', 'email', 'token', 'user_id']
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
-
-    def validate(self, data):
-        user = authenticate(**data)
-        if user and user.is_active:
-            return user
-        raise serializers.ValidationError("Incorrect credentials.")
+        fields = ('name', 'hotel', 'max_occupacy')
 
 class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
-        fields = ['id', 'user', 'hotel', 'room_category', 'check_in_date', 'check_out_date', 'num_guests']
-
+        fields = ('hotel', 'user', 'room_category', 'check_in_date', 'check_out_date', 'num_guests')
