cf66dee3463df9b3a48bbd2408a73c9a710fc50c
Mantiena <mantasdagg@gmail.com>
2022-12-12 12:21:14 +0200

    created new serializers modifications.

--- hotel_reservation_system/reservation/views.py
+++ hotel_reservation_system/reservation/views.py
@@ -1,127 +1,170 @@
-from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from django.core.exceptions import ValidationError
-from rest_framework import generics, status, permissions, authentication
 from rest_framework.views import APIView
+from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.authtoken.models import Token
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
-from .models import Hotel, RoomCategory, Reservation
-from .serializers import HotelSerializer, RoomCategorySerializer, ReservationSerializer, UserRegistrationSerializer, UserSerializer, LoginSerializer
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 
-
-class HotelListView(generics.ListAPIView):
-    queryset = Hotel.objects.all()
-    serializer_class = HotelSerializer
-
-class HotelDetailView(generics.RetrieveAPIView):
-    queryset = Hotel.objects.all()
-    serializer_class = HotelSerializer
-
-class RoomCategoryListView(generics.ListAPIView):
-    queryset = RoomCategory.objects.all()
-    serializer_class = RoomCategorySerializer
-
-class RoomCategoryDetailView(generics.RetrieveAPIView):
-    queryset = RoomCategory.objects.all()
-    serializer_class = RoomCategorySerializer
-
-class ReservationListView(APIView):
-    permission_classes = (IsAuthenticated)
-
-    def get(self, request):
-        reservations = Reservation.objects.filter(user=request.user)
-        serializer = ReservationSerializer(reservations, many=True)
-        return Response(serializer.data)
-
-class ReservationDetailView(APIView):
-    permission_classes = (IsAuthenticated)
+class UserDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
 
     def get(self, request, pk):
-        # Get the reservation with the given ID
-        reservation = get_object_or_404(Reservation, pk=pk)
-
-            # Only allow the user to see their own reservation
-        if reservation.user != request.user:
-            return Response(status=403)
-
-        serializer = ReservationSerializer(reservation)
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
         return Response(serializer.data)
 
     def put(self, request, pk):
-        reservation = get_object_or_404(Reservation, pk=pk)
-
-        if reservation.user != request.user:
-            return Response(status = 403)
-        
-        serializer = ReservationSerializer(reservation, data=request.data)
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=400)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        reservation = get_object_or_404(Reservation, pk=pk)
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-        if reservation.user != request.user:
-            return Response(status=403)
+
+class UserListView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+
+
+
+
+
+# from django.shortcuts import render, get_object_or_404
+# from django.contrib.auth.models import User
+# from django.contrib.auth import authenticate
+# from django.core.exceptions import ValidationError
+# from rest_framework import generics, status, permissions, authentication
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework.permissions import IsAuthenticated
+# from rest_framework.authtoken.models import Token
+# from rest_framework.authentication import TokenAuthentication
+# from rest_framework.permissions import IsAuthenticated
+# from rest_framework.authentication import BasicAuthentication
+# from rest_framework.response import Response
+# from rest_framework.decorators import api_view, permission_classes, authentication_classes
+# from .models import Hotel, RoomCategory, Reservation
+# from .serializers import HotelSerializer, RoomCategorySerializer, ReservationSerializer, UserRegistrationSerializer, UserSerializer, LoginSerializer
+
+
+# class HotelListView(generics.ListAPIView):
+#     queryset = Hotel.objects.all()
+#     serializer_class = HotelSerializer
+
+# class HotelDetailView(generics.RetrieveAPIView):
+#     queryset = Hotel.objects.all()
+#     serializer_class = HotelSerializer
+
+# class RoomCategoryListView(generics.ListAPIView):
+#     queryset = RoomCategory.objects.all()
+#     serializer_class = RoomCategorySerializer
+
+# class RoomCategoryDetailView(generics.RetrieveAPIView):
+#     queryset = RoomCategory.objects.all()
+#     serializer_class = RoomCategorySerializer
+
+# class ReservationListView(APIView):
+#     permission_classes = (IsAuthenticated)
+
+#     def get(self, request):
+#         reservations = Reservation.objects.filter(user=request.user)
+#         serializer = ReservationSerializer(reservations, many=True)
+#         return Response(serializer.data)
+
+# class ReservationDetailView(APIView):
+#     permission_classes = (IsAuthenticated)
+
+#     def get(self, request, pk):
+#         # Get the reservation with the given ID
+#         reservation = get_object_or_404(Reservation, pk=pk)
+
+#             # Only allow the user to see their own reservation
+#         if reservation.user != request.user:
+#             return Response(status=403)
+
+#         serializer = ReservationSerializer(reservation)
+#         return Response(serializer.data)
+
+#     def put(self, request, pk):
+#         reservation = get_object_or_404(Reservation, pk=pk)
+
+#         if reservation.user != request.user:
+#             return Response(status = 403)
         
-        reservation.delete()
-        return Response(status=204)
+#         serializer = ReservationSerializer(reservation, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=400)
 
-class UserRegistrationView(generics.CreateAPIView):
-    serializer_class = UserRegistrationSerializer
+#     def delete(self, request, pk):
+#         reservation = get_object_or_404(Reservation, pk=pk)
 
-class UserDetailView(generics.RetrieveAPIView):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
+#         if reservation.user != request.user:
+#             return Response(status=403)
+        
+#         reservation.delete()
+#         return Response(status=204)
 
-class UserUpdateView(generics.UpdateAPIView):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
+# class UserRegistrationView(generics.CreateAPIView):
+#     serializer_class = UserRegistrationSerializer
 
-    def perform_update(self, serializer):
-        user = self.request.user
-        password = self.request.data.get('password')
+# class UserDetailView(generics.RetrieveAPIView):
+#     serializer_class = UserSerializer
+#     queryset = User.objects.all()
 
-        if password and user.id == self.kwargs['pk']:
-            if not user.check_password(password):
-                raise ValidationError({'password': 'Incorrect password'})
-        serializer.save()
+# class UserUpdateView(generics.UpdateAPIView):
+#     serializer_class = UserSerializer
+#     queryset = User.objects.all()
 
+#     def perform_update(self, serializer):
+#         user = self.request.user
+#         password = self.request.data.get('password')
 
-class UserDestroyView(generics.DestroyAPIView):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
-
-    def get_object(self):
-        return self.request.user()
-
-class LoginView(generics.GenericAPIView):
-    serializer_class = LoginSerializer
-    authentication_classes = (authentication.BasicAuthentication,)
-    permission_classes = (permissions.IsAuthenticated,)
-
-    def post(self, request):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data
-        # Generate a new token for the user
-        token, _ = Token.objects.get_or_create(user=user)
-        return Response({"token": token.key})
+#         if password and user.id == self.kwargs['pk']:
+#             if not user.check_password(password):
+#                 raise ValidationError({'password': 'Incorrect password'})
+#         serializer.save()
 
 
-class LogoutView(APIView):
-    authentication_classes = (TokenAuthentication, BasicAuthentication)
-    permission_classes = [IsAuthenticated]
+# class UserDestroyView(generics.DestroyAPIView):
+#     serializer_class = UserSerializer
+#     queryset = User.objects.all()
 
-    def post(self, request):
-        user = request.user
-        user.auth_token.delete()
-        return Response(status=status.HTTP_200_OK)
+#     def get_object(self):
+#         return self.request.user()
+
+# class LoginView(generics.GenericAPIView):
+#     serializer_class = LoginSerializer
+#     authentication_classes = (authentication.BasicAuthentication,)
+#     permission_classes = (permissions.IsAuthenticated,)
+
+#     def post(self, request):
+#         serializer = self.get_serializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         user = serializer.validated_data
+#         # Generate a new token for the user
+#         token, _ = Token.objects.get_or_create(user=user)
+#         return Response({"token": token.key})
+
+
+# class LogoutView(APIView):
+#     authentication_classes = (TokenAuthentication, BasicAuthentication)
+#     permission_classes = [IsAuthenticated]
+
+#     def post(self, request):
+#         user = request.user
+#         user.auth_token.delete()
+#         return Response(status=status.HTTP_200_OK)
