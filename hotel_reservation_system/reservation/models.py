cba98e3a1c8f3b1988c2de9dc1084fa97e28f78b
Mantiena <mantasdagg@gmail.com>
2022-12-12 12:21:01 +0200

    creating new views.py

--- hotel_reservation_system/reservation/models.py
+++ hotel_reservation_system/reservation/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
-
-# Create your models here.
-
 class Hotel(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
