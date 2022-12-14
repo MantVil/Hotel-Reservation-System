7000f8bea0d9a7cf1937d12c116688634aae0893
Mantiena <mantasdagg@gmail.com>
2022-12-14 16:50:04 +0200

    changes

--- hotel_reservation_system/reservation/models.py
+++ hotel_reservation_system/reservation/models.py
@@ -31,7 +31,7 @@ class RoomCategory(models.Model):
 
 class Reservation(models.Model):
     hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reservations")
     room_category = models.ForeignKey(RoomCategory, on_delete=models.CASCADE, related_name ='reservations', default=None)
     check_in_date = models.DateField()
     check_out_date = models.DateField()
--- hotel_reservation_system/reservation/serializers.py
+++ hotel_reservation_system/reservation/serializers.py
@@ -4,14 +4,14 @@ from .models import Hotel, RoomCategory, Reservation
 class HotelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hotel
-        fields = ('id', 'name', 'address')
+        fields = ('id', 'user_id' 'name', 'address')
 
 class RoomCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = RoomCategory
-        fields = ('id' 'name', 'hotel', 'max_occupacy')
+        fields = ('id', 'user_id', 'name', 'hotel', 'max_occupacy')
 
 class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
-        fields = ('id', 'hotel', 'user', 'room_category', 'check_in_date', 'check_out_date', 'num_guests')
+        fields = ('id', 'hotel', 'user_id', 'room_category', 'check_in_date', 'check_out_date', 'num_guests')
